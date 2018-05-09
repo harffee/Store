d3d205daf8af51dd5ad01c4ccca2475b474c12a1
v-jzingh <yzzj1126@qq.com>
2018-05-09 17:48:27 +0800

    just set up the local db

---
+++ Store/Store.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Store.Domain.Entities;
+using System.Data.Entity;
+
+namespace Store.Domain.Concrete
+{
+    public class EFDbContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Store/Store.Domain/Concrete/EFProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Store.Domain.Abstract;
+using Store.Domain.Entities;
+using System.Linq;
+
+namespace Store.Domain.Concrete
+{
+    public class EFProductRepository:IProductRepository
+    {
+        private EFDbContext context = new EFDbContext();
+        public IQueryable<Product> Products
+        {
+            get { return context.Products; }
+        }
+    }
+}
