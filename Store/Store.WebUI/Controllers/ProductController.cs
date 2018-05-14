995909aa4c9c6b1b5336f62186240d5c9b6e91d6
v-jzingh <yzzj1126@qq.com>
2018-05-14 17:26:40 +0800

    PageSize

--- Store/Store.WebUI/Controllers/ProductController.cs
+++ Store/Store.WebUI/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Store.Domain.Abstract;
 using Store.Domain.Entities;
+using Store.WebUI.Models;
 
 namespace Store.WebUI.Controllers
 {
@@ -12,13 +13,29 @@ namespace Store.WebUI.Controllers
     {
 
         private IProductRepository repository;
+        public int pageSize = 4;
         public ProductController(IProductRepository productRepository)
         {
             this.repository = productRepository;
         }
-        public ViewResult List()
+        public ViewResult List(int page=1)
         {
-            return View(repository.Products);
+            //return View(repository.Products.OrderBy(p=>p.ProductID)
+            //                               .Skip((page-1)*pageSize)
+            //                               .Take(pageSize));
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = repository.Products.OrderBy(p => p.ProductID)
+                                            .Skip((page - 1) * pageSize)
+                                            .Take(pageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = pageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+            return View(model);
         }
     }
 }
