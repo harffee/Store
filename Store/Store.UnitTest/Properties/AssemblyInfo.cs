5c7fff99a04dee25fb62bcb2011e5caa63557139
v-jzingh <yzzj1126@qq.com>
2018-05-18 16:55:47 +0800

    Cart ok

---
+++ Store/Store.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Store.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft Corp.")]
+[assembly: AssemblyProduct("Store.UnitTest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft Corp. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("527d2f2b-f515-401a-bed0-be0bcbc95a7b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Store/Store.WebUI/Controllers/CartController.cs
+++ Store/Store.WebUI/Controllers/CartController.cs
@@ -59,26 +59,26 @@ namespace Store.WebUI.Controllers
         //    return View(new ShippingDetails());
         //}
 
-        public RedirectToRouteResult AddToCart(Cart cart, int productId, string returnUrl)
+        public RedirectToRouteResult AddToCart(int productId, string returnUrl)
         {
             Product product = repository.Products
                 .FirstOrDefault(p => p.ProductID == productId);
 
             if (product != null)
             {
-                cart.AddItem(product, 1);
+                GetCart().AddItem(product, 1);
             }
             return RedirectToAction("Index", new { returnUrl });
         }
 
-        public RedirectToRouteResult RemoveFromCart(Cart cart, int productId, string returnUrl)
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
         {
             Product product = repository.Products
                 .FirstOrDefault(p => p.ProductID == productId);
 
             if (product != null)
             {
-                cart.RemoveLine(product);
+                GetCart().RemoveLine(product);
             }
             return RedirectToAction("Index", new { returnUrl });
         }
